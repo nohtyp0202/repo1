2850c134374eb7a4baffcd84733ba13cf23d5f62
nohtyp0202 <accutesting19@gmail.com>
2020-05-24 16:20:10 -0500

    List practice excercises added

---
+++ Lists_Practice_excercises.py
@@ -0,0 +1,16 @@
+# 1. Write a Python program to sum all the items in a list.
+
+# pseudo code:
+# make a list of numbers
+# Create a variable and initialize it to 0
+# iterate through the list and add all the items
+# print the sum
+
+num_list = [1,2,3,4,5,6,7,8,9]
+
+sum = 0
+
+for i in num_list:
+    sum = sum + i
+
+print sum
